e0ae82599a9a9ece9c693fd3c53f46e3e92e1559
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-01-21 10:43:31 +0400

    editing public folder

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./css/detailed.css";
+import img from "./img/cheesePizza.png";
+import {renderDetailedPage} from "./js/views/detaild.js";
+renderDetailedPage();
---
+++ src/js/views/detaild.js
@@ -0,0 +1,82 @@
+const renderDetailedPage = () => {
+  document.querySelector(".container").innerHTML = `  <div class="container">
+    <div class="product">
+      <div class="productImg">
+        <img src="../../img/cheesePizza.png" alt="cheesePizza">
+      </div>
+      <div class="productName">
+        <h2 class="detailedProductName">Cheese Pizza</h2>
+        <label for="">1pcs:</label>
+        <label class="productPrice" for="">֏350</label>
+      </div>
+      <h2>Ingredients</h2>
+      <ul class="ingredientsList">
+        <li>Chees</li>
+        <li>Tomato</li>
+        <li>Pepper</li>
+      </ul>
+    </div>
+
+  <div class="product">
+    <div class="productImg">
+      <img src="../../img/pepperoniPizza.png" alt="cheesePizza">
+    </div>
+    <div class="productName">
+      <h2 class="detailedProductName">Pepperoni Pizza</h2>
+      <label for="">1pcs:</label>
+      <label class="productPrice" for="">֏350</label>
+    </div>
+    <h2>Ingredients</h2>
+    <ul class="ingredientsList">
+      <li>Chees</li>
+      <li>Tomato</li>
+      <li>Pepper</li>
+    </ul>
+
+  </div>
+
+ <div class="product">
+    <div class="productImg">
+      <img src="../../img/cheeseTomatoPizza.png" alt="cheesePizza">
+    </div>
+    <div class="productName">
+      <h2 class="detailedProductName">CheeseTomato Pizza</h2>
+      <label for="">1pcs:</label>
+      <label class="productPrice" for="">֏350</label>
+    </div>
+    <h2>Ingredients</h2>
+    <ul class="ingredientsList">
+      <li>Chees</li>
+      <li>Tomato</li>
+      <li>Pepper</li>
+    </ul>
+
+    </div>
+  
+</div>
+    <!-- end products container -->
+
+<div class="fiterContainer">
+        <div class="filter">Filter</div>
+        <!-- end filter button -->
+        <div class="filterWindow hidden">
+          <div class="filterInputBlock">
+            <input type="text" class="searchIngredient" placeholder="Search for ingredients..">
+            <!-- <h2 class="filterClose">X</h2> ??????????-->
+          </div>
+
+          <ul class="filterItem">
+            <li>Chees</li>
+            <li>Tomato</a></li>
+            <li>Ketchup</li>
+            <li>Mushroom</li>
+            <li>Maiones</li>
+          </ul>
+        </div>
+        <!-- end filter window -->
+</div>`;
+
+};
+export {
+  renderDetailedPage
+};
