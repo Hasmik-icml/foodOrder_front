ad863fde28abf708c3971ef856e0892fcce7afaf
seda <shahnazaryan.seda@mail.ru>
2022-01-21 15:35:15 +0400

    modified register and products js files

--- src/js/views/products.js
+++ src/js/views/products.js
@@ -1,10 +1,10 @@
 const renderProductsPage=()=>{
-  const container= ` <div class="grid-item">Տաք ուտեստ</div>
+  const container= ` <div class="containerProducts" ><div class="grid-container"><div class="grid-item">Տաք ուտեստ</div>
     <div class="grid-item">Խորտիկներ</div>
     <div class="grid-item">Քաղցրավենիք</div>
     <div class="grid-item">Ըմպելիք</div>
     <div class="grid-item">Պիցցաներ</div>
-    <div class="grid-item">Աղցաններ</div>`
+    <div class="grid-item">Աղցաններ</div></div></div>`
     document.querySelector(".container").innerHTML = container;
 };
 export { renderProductsPage };
--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,12 +1,12 @@
 const renderRegisterPage =()=>{
-  console.log(4)
-    const container =` <select name="tables" id="selectTable" class="table-name">
+  
+    const container =`<div class="containerReg"> <select name="tables" id="selectTable" class="table-name">
     <option value="Սեղանի համար" class="tableNumber"selected>Սեղանի համար</option>  
     <option value="1">1</option>  
     <option value="2">2</option>
     <option value="3">3</option>
   </select> 
-  <button class="confirm-btn">Ամրագրել</button> `
+  <button class="confirm-btn">Ամրագրել</button></div> `
   document.querySelector(".container").innerHTML = container;
 };
 
