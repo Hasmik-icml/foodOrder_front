51e5836a34f373da2aec3f9e7c7c2b93c1ca192e
anniejoel <annhayriyan@gmail.com>
2022-01-21 17:27:56 +0400

    added specificproduct pages not comleted

--- src/js/views/specificProduct.js
+++ src/js/views/specificProduct.js
@@ -1,5 +1,5 @@
 const specificProductPage = () => {
-    document.querySelector(".container").innerHTML = ` 
+    document.querySelector(".container").innerHTML = ` <div id="mainContainer">
  <nav>
     <div id="menuToggle">
         <input type="checkbox" />
@@ -24,8 +24,8 @@ const specificProductPage = () => {
                 <img class="spcProductImg" src="img/pepperoniPizza.png" alt="pepperoniPizza">
             </div>
             <div class="price">
-                <label>Գին՝</label>
-                <label class="productPrice">֏350</label>
+                <label>Գին՝ ֏</label>
+                <label class="productPrice">350</label>
             </div>
         </div>
         <div class="item3">
@@ -35,24 +35,50 @@ const specificProductPage = () => {
             <label for="ingredient1"> Պանիր </label><br>
             <input type="radio" id="ingredient2" name="ingredient2" value="pepperoni">
             <label for="ingredient2"> Երշիկ </label><br>
-            <input type="radio" id="ingredient3" name="ingredient3" value="tomato">
+            <input type="radio" id="ingredient3" name="ingredient3" value="tomato"> 
             <label for="ingredient3"> Լոլիկ </label>
             </div>
             <div class="numberOfProduct">
                 <h4>Քանակ</h4>
                 <div class="quantityOfProduct"> 
                 <label>Կտոր</label>
-                 <button class="subtract">-</button>
-                <input type="text" class="quantity" id value="0" name="quantity">
-                <button class="add">+</button>
+                 <button id="subtract">-</button>
+                <input type="text" id="quantity" value="0" name="quantity">
+                <button id="add">+</button> 
+                 </div>
+                 <div class="totalPriceDiv">
+                  <label>Ընդհանուր գին</label>
+                <input type="text" id="totalPrice" value="0" name="quantity">
                 </div>
                
             </div>
             <button class="addToBasket">Ավելացնել զամբյուղ</button>
         </div>
 
+         </div>
     </div>
     `;
+    let quantity = document.getElementById("quantity");
+    document.getElementById("add").addEventListener("click", ()=>{
+        quantity.value++;
+        totalPrice.value = initialPrice * quantity.value; // si pongo el numero, no cambia auto..
+    });
+    document.getElementById("subtract").addEventListener("click", ()=>{
+        if(quantity.value>0)
+        {quantity.value--;
+        totalPrice.value = initialPrice * quantity.value;
+        }
+    });
+    console.log(quantity.value);
+    let initialPrice = document.querySelector(".productPrice").innerHTML;
+    console.log( initialPrice);
+    let totalPrice = document.getElementById("totalPrice");
+
+
 };
-document.querySelector(".add").addEventListener("click", () => { document.getElementsByClassName() })
+
+
+
+
+
 export {specificProductPage};
