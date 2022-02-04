1a685fc8ae3dc5228222619a8c2cfe22eae46217
seda <shahnazaryan.seda@mail.ru>
2022-02-04 21:04:45 +0400

    fetch

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -6,11 +6,13 @@ const renderRegisterPage =()=>{
         =`<div class="containerReg">
  <select name="tables" id="selectTable" class="table-name">
     <option class="tableNumber" hidden>Սեղանի համար</option>  
-    <option value="1">1</option>  
-    <option value="2">2</option>
-    <option value="3">3</option>
+   
   </select> 
   <button class="confirm-btn">Ամրագրել</button></div> `
+
+  
+
+
 
     registerEventListeners();
 }
