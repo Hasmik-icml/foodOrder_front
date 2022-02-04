458157daa22d3d02f5c58c9f5f5008d61788e67a
anniejoel <annhayriyan@gmail.com>
2022-02-04 19:55:14 +0400

    changed register and product js <=> database

--- src/js/views/products.js
+++ src/js/views/products.js
@@ -6,17 +6,38 @@ import {
 } from "../helpers/eventListeners";
 const renderProductsPage=()=>{
   document.querySelector(".container").innerHTML
-      += ` <div class="containerProducts" ><div class="grid-container">
-    <div id="hot_dishes" class="grid-item ">Տաք ուտեստ</div>
+      += ` <div class="containerProducts" >
+                <div class="grid-container">
+ 
+                </div>
+        </div>`
+
+fetch("http://localhost:8080/productType?url=get-all")
+    .then(function(response){
+      return response.json()
+    }).then(function(data){
+  let type = data.reduce((acc,current) => {
+    return acc += `<div id="${current.name}" class="grid-item">${current.name}</div> `
+  }, "");
+
+  document.querySelector(".grid-container")
+      .insertAdjacentHTML("beforeend", type);
+})
+
+
+  basketPageEventListener();
+  cancelOrder();
+  pageBackEventListener();
+  productsEventListeners();
+
+}
+
+export { renderProductsPage };
+
+/*   <div id="hot_dishes" class="grid-item ">Տաք ուտեստ</div>
+
     <div id="snacks" class="grid-item " >Խորտիկներ</div>
     <div id="candies" class="grid-item " >Քաղցրավենիք</div>
     <div id="drinks" class="grid-item " >Ըմպելիք</div>
     <div id="pizzas" class="grid-item ">Պիցցաներ</div>
-    <div id="salads" class="grid-item ">Աղցաններ</div></div></div>`
-
-  productsEventListeners();
-  basketPageEventListener();
-  cancelOrder();
-  pageBackEventListener();
-};
-export { renderProductsPage };
+    <div id="salads" class="grid-item ">Աղցաններ</div>*/
--- src/js/views/register.js
+++ src/js/views/register.js
@@ -6,15 +6,27 @@ const renderRegisterPage =()=>{
         =`<div class="containerReg">
  <select name="tables" id="selectTable" class="table-name">
     <option class="tableNumber" hidden>Սեղանի համար</option>  
-    <option value="1">1</option>  
-    <option value="2">2</option>
-    <option value="3">3</option>
+  
   </select> 
   <button class="confirm-btn">Ամրագրել</button></div> `
+
+    fetch("http://localhost:8080/table?url=get-all")
+        .then(function (response){
+            return response.json()
+        }).then(function(data){
+            let id = data.reduce((acc,current) => {
+                return acc += `<option value="${current.id}">${current.number}</option> `
+        }, "");
+            document.getElementById("selectTable")
+                .insertAdjacentHTML("beforeend", id);
+    })
+
 
     registerEventListeners();
 }
 
 export { renderRegisterPage };
 
-
+/*<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>*/
