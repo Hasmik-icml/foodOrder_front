dec48c175179e18d5e7af3c95ddd69a5f9727659
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-02-05 14:22:40 +0400

    modified register page

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,9 +1,12 @@
 import router from "../routing";
-import {registerEventListeners} from "../helpers/eventListeners"
-import {setCookie} from "../helpers/storage";
-const renderRegisterPage =()=>{
-    document.querySelector(".container").innerHTML
-        =`<div class="containerReg">
+import {
+    registerEventListeners
+} from "../helpers/eventListeners"
+import {
+    setCookie
+} from "../helpers/storage";
+const renderRegisterPage = () => {
+    document.querySelector(".container").innerHTML = `<div class="containerReg">
  <select name="tables" id="selectTable" class="table-name">
     <option class="tableNumber" hidden>Սեղանի համար</option>  
   
@@ -11,21 +14,25 @@ const renderRegisterPage =()=>{
   <button class="confirm-btn">Ամրագրել</button></div> `
 
     fetch("http://localhost:8080/table?url=get-all")
-        .then(function (response){
-            return response.json()
-        }).then(function(data){
-            let id = data.reduce((acc,current) => {
+        .then(function (response) {
+            console.log("response", response);
+            return response.json();
+        }).then(function (data) {
+            console.log("data", data);
+            let id = data.reduce((acc, current) => {
                 return acc += `<option value="${current.id}">${current.number}</option> `
-        }, "");
+            }, "");
             document.getElementById("selectTable")
                 .insertAdjacentHTML("beforeend", id);
-    })
+        })
 
 
     registerEventListeners();
 }
 
-export { renderRegisterPage };
+export {
+    renderRegisterPage
+};
 
 /*<option value="1">1</option>
 <option value="2">2</option>
